17cc63db6c409ce89b0c9d768b435b4b2b5b6d34
SRINJOY DAS <srinjoydas1104@gmail.com>
2025-02-27 01:38:33 +0530

    port config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 
 
-export const BACKEND_URL = 'http://ec2-16-171-116-191.eu-north-1.compute.amazonaws.com:3000'
+export const BACKEND_URL = 'http://ec2-16-171-116-191.eu-north-1.compute.amazonaws.com:8080'
